95ae9fb34fb98b9961ab8cdae66ecb73692a7d02
Ricardo Almeida <ricardo2675@proton.me>
2024-11-18 23:30:07 +0000

    Data can now be stored and accessed from a json file

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use serde::{Deserialize, Serialize};
+use std::{fs::File, io::BufReader};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BudgetItem {
+    name: String,
+    amount: f64,
+    category: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("database.json").expect("Unable to open file");
+    let reader = BufReader::new(file);
+
+    let budget_items: Vec<BudgetItem> = serde_json::from_reader(reader)?;
+
+    for item in budget_items {
+        println!("{:?}", item);
+    }
+
+    Ok(())
+}
